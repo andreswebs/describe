f4dd36664b5de7c547e8a21dfe044716d964f457
andreswebs <andresil@pm.me>
2020-12-12 23:30:05 -0300

    update description

--- mod.test.ts
+++ mod.test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "./deps.test.ts";
 import { description } from "./mod.ts";
 
 const aDescribedUnit = {
-  name: "this line is the unit name (what is under testing)",
+  name: "--> this line is the unit name (what is under testing) <--",
   given: "a description object",
   should: "return a formatted description string",
 };
@@ -10,6 +10,6 @@ const aDescribedUnit = {
 Deno.test(description(aDescribedUnit), () => {
   const actual = description(aDescribedUnit);
   const expected =
-    `this line is the unit name (what is under testing):\n  given: a description object\n  should: return a formatted description string\n`;
+    `--> this line is the unit name (what is under testing) <--:\n  given: a description object\n  should: return a formatted description string\n`;
   assertEquals(actual, expected);
 });
